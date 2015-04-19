f1a749d4c44c8d030f6e9410b332c13025babc66
Cosmin Banu <cosminb@gmail.com>
2015-04-19 21:07:27 +0300

    Print stats for each tmpfs/ext4 mount point. Some fixes.

--- part.c
+++ part.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <mntent.h>
+#include <sys/statvfs.h>
+
 int main() {
-	printf("part\n");
+	printf("'df' PoC\n");
 	FILE* f = setmntent("/proc/mounts", "r");
+	struct statvfs svfs;
 	struct mntent *me;
 	while (NULL != (me = getmntent(f))) {
 		if ((0 == strcmp(me->mnt_type, "tmpfs")) ||
-			(0 == strcmp(me->mnt_type, "ext4i"))) {
+			(0 == strcmp(me->mnt_type, "ext4"))) {
+			printf("fsname=%s dir=%s type=%s\n", me->mnt_fsname, me->mnt_dir, me->mnt_type);
+			memset(&svfs, 0, sizeof(svfs));
+			if (0 == statvfs(me->mnt_dir, &svfs)) {
+				printf("bsize %lu\n", svfs.f_bsize);
+				printf("blocks %lu\n", svfs.f_blocks);
+				printf("bfree %lu\n", svfs.f_bfree);
+				printf("bavail %lu\n", svfs.f_bavail);
+			}
 		}
-		printf("fsname=%s dir=%s type=%s\n", me->mnt_fsname, me->mnt_dir, me->mnt_type);
 	}
 	endmntent(f);
 	f = NULL;
